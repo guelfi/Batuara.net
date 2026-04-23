e7195c1149f9b941cdde7736173e1e7a38efaa31
Marco Guelfi <marco.guelfi@meurh360.com.br>
2026-04-22 21:12:51 -0300

    chore(database): remover arquivos obsoletos (README, gen_hash.js, reset_password*.sql)
    - README.md documentava backup_batuara_20260403.dump que foi removido
    - gen_hash.js: utilitario bcrypt trivial, regeneravel (expunha senha padrao)
    - reset_password.sql: versao intermediaria, superada por _final
    - reset_password_final.sql: hash datado sem uso operacional corrente

--- database/reset_password.sql
+++
@@ -1,1 +0,0 @@
-UPDATE batuara.users SET password_hash = '$2a$10$jdRtqvNX2ou9Xesh/lfqFux12gezvTJaAdHMvJnsw/mG1FxjScKji' WHERE id = 2;
--- database/reset_password_final.sql
+++
@@ -1,1 +0,0 @@
-UPDATE batuara.users SET password_hash = '$2a$11$x8XPWfptjNw7IfjvySSCP.7CjWqlQAy18KXmSA7niSHlyrHNJkcGu' WHERE id = 2;
